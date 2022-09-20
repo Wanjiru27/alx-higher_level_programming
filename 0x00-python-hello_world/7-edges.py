db965316a0bc28bb9ca70d6afc8537480eb99f5a
Wanjiru27 <mwanjiru736@gmail.com>
2022-09-20 10:16:36 -0700

    Hello world

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"{word_first_3 [0:4]}")
-print(f"{word_last_2 [2:4]}")
-print(f"{middle_word [8:10]}")
+print(f"{word [0:3]}")
+print(f"{word [7: ]}")
+print(f"len ({word [1:-1]})")
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
